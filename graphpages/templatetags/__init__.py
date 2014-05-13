e7812d709f9d1439959a0a0545836d347d2273f0
Richard Bell <rbell01824@gmail.com>
2014-05-13 14:17:43 -0400

    WIP. Code cleanup.

--- graphpages/templatetags/__init__.py
+++ graphpages/templatetags/__init__.py
@@ -17,3 +17,14 @@ __date__ = '2/23/14'
 __license__ = "All rights reserved"
 __version__ = "0.1"
 __status__ = "dev"
+
+########################################################################################################################
+
+# Load the default template tags.
+# This insures that custom template tags are always available to Django's
+# template render process.
+
+# fixme: this shouldn't be needed, look into it.  django pages should override
+
+from djangopages.dpage import load_templatetags
+load_templatetags()
