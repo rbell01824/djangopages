73e2fb2d698a821a5bb425c68e3cdeda10df3cd6
rbell01824@gmail.com <rbell01824@gmail.com>
2014-08-10 18:03:43 -0400

    WIP. Added __str__ and __repr__ to Content.

--- djangopages/dpage.py
+++ djangopages/dpage.py
@@ -202,6 +202,13 @@ class Content(object):
     def __mul__(self, other):
         return self.render() * other
 
+    def __str__(self):
+        return self.render()
+
+    def __repr__(self):
+        return self.render()
+
+
 ########################################################################################################################
 #
 # Support Routines
